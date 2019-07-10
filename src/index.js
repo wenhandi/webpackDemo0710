467f1fcf2678320017b0e9f7c28fbebe9f5b1eed
xupan <18233566286@163.com>
2019-07-10 22:21:16 +0800

    依次通过css-loader/style-loader处理.css资源文件

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // 声明模块所需的依赖，webpack能够利用这些信息构建依赖图，然后使用图生成一个优化过的，会以正确顺序执行的bundle
 import _ from 'lodash'
+import './style.css'
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+
     return element;
   }
   

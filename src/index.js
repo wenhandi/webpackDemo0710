f6dcbeffc2ab6e244d35dee764dbaf2c20f60b4e
xupan <18233566286@163.com>
2019-07-11 21:45:21 +0800

    htmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 // 声明模块所需的依赖，webpack能够利用这些信息构建依赖图，然后使用图生成一个优化过的，会以正确顺序执行的bundle
 import _ from 'lodash'
-import './style.css'
-import Gongqj from './gongqijun.png'
-import Data from './data.xml'
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button')
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    
+    btn.innerHTML = "click and console";
+    btn.onclick = printMe;
+    
 
-    var admireImg = new Image()
-    admireImg.src = Gongqj
+    element.appendChild(btn)
 
-    element.appendChild(admireImg)
-
-    console.log(Data)
 
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWbpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWbpackPlugin({
+            title: '输出管理插件'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
 }

2ff1dc952709e60f06f4bf5caa70e634ad60e4f7
xupan <18233566286@163.com>
2019-07-17 22:32:12 +0800

    merge

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
-// 声明模块所需的依赖，webpack能够利用这些信息构建依赖图，然后使用图生成一个优化过的，会以正确顺序执行的bundle
-import _ from 'lodash'
-import printMe from './print'
-import './styles.css'
+import { cube } from './math.js'
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button')
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    btn.innerHTML = "click and console";
-    btn.onclick = printMe;
-    
+  var element = document.createElement('pre');
 
-    element.appendChild(btn)
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = [
+    'hello webpack',
+    '5的立方等于' + cube(5)
+  ].join('\n\n')
 
+  return element;
+}
+let element = component()
+document.body.appendChild(element);
 
-    return element;
-  }
-  let element = component()
-  document.body.appendChild(element);
-
-  if(module.hot){
-    module.hot.accept('./print.js', function(){
-      console.log("接收更新的printMe模块")
-      // printMe()
-      document.body.removeChild(element);
-      element = component();
-      document.body.appendChild(element)
-
-    })
-  }

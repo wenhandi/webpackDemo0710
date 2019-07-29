1691018c63d4336c9eb9897761e563926f940a71
xupan <18233566286@163.com>
2019-07-29 17:53:45 +0800

    环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('Html-webpack-plugin')
-const {CleanWebpackPlugin} = require('Clean-webpack-plugin')
-const WorkboxPlugin = require('Workbox-webpack-plugin')
+const path = require('path')
 
-module.exports = {
-    entry: {
-        index: './src/index.ts'
-    },
-    // 告诉webpack提取source map 比行内镰刀最终的bundle中
-    devtool: 'inline-source-map',
-    module: {
-        rule: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+module.exports = env => {
+    console.log('NODE_ENV   ', env.NODE_ENV)
+    console.log('production   ', env.production)
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        }
+    }
 }
